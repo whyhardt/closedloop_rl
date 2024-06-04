0ad5e9e51c5adc69b04669c19bae7276ff1f9c85
whyhardt <daniel.weinhardt@uni-osnabrueck.de>
2024-06-04 12:11:29 +0200

    adjusted agentnetwork a bit

--- resources/bandits.py
+++ resources/bandits.py
@@ -259,11 +259,7 @@ class AgentNetwork:
 
   def get_choice_probs(self) -> np.ndarray:
     """Predict the choice probabilities as a softmax over output logits."""
-    output_logits, _ = self._model_fun(self._xs, self._state)
-    output_logits = np.array(output_logits)
-    output_logits = output_logits[0][:self._n_actions]
-    # choice_probs = np.exp(output_logits) / np.sum(np.exp(output_logits))
-    # choice_probs = 1 / (1 + np.exp(output_logits[1] - output_logits[0]))
+    output_logits = np.array(self._state[-1]).reshape(-1)
     choice_probs = jax.nn.softmax(output_logits)
     return np.array(choice_probs)
 
