5f59d388ff4ea507e89cad56021b7a3433dbfe9c
malte <lebemalte@gmail.com>
2025-07-03 15:04:01 +0200

    Added new training loop with meta optimization

---
+++ meta_opt_pipeline_test.py
@@ -0,0 +1,114 @@
+import sys
+import os
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore", message="pkg_resources is deprecated.*")
+
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import pipeline_rnn
+from resources.rnn import RLRNN, RLRNN_dezfouli2019, RLRNN_dezfouli2019_blocks, RLRNN_eckstein2022#, RLRNN_meta_eckstein2022, RLRNN_eckstein2022_FC
+
+
+# -------------------------------------------------------------------------------
+# SPICE CONFIGURATIONS
+# -------------------------------------------------------------------------------
+
+path_model = 'params/T1-T2-TEST.pkl'
+path_data = 'data/eckstein2022/eckstein2022.csv'
+train_test_ratio = 0.7
+class_rnn = RLRNN_eckstein2022
+additional_inputs = None
+# class_rnn = RLRNN_meta_eckstein2022
+# additional_inputs = ['age']
+
+# class_rnn = RLRNN_dezfouli2019
+# train_test_ratio = None#[3, 6, 9]#[1, 3, 4, 6, 8, 10]   # list of test sessions
+# path_model = 'params/dezfouli2019/rnn_dezfouli2019_no_l1_l2_0_crossval_baseline.pkl'
+# path_data = 'data/dezfouli2019/dezfouli2019_simulated_gql_multi_session_d1.csv'
+# additional_inputs = None
+
+# class_rnn = RLRNN_dezfouli2019_blocks
+# train_test_ratio = [1, 3, 4, 6, 8, 10]  # list of test sessions
+# path_model = 'params/dezfouli2019/rnn_dezfouli2019_blocks_rldm_l1emb_0_001_l2_0_0001.pkl'
+# path_data = 'data/dezfouli2019/dezfouli2019.csv'
+# additional_inputs = None
+
+# -------------------------------------------------------------------------------
+# SPICE PIPELINE
+# -------------------------------------------------------------------------------
+
+_, loss, histories = pipeline_rnn.main(
+    
+    # sparsification parameter
+    l1_weight_decay=0.,
+    # generalization parameters
+    l2_weight_decay=0.0, # Should now auto adapt using T1-T2
+    dropout=0.25,
+    train_test_ratio=train_test_ratio,
+    
+    # general training parameters
+    checkpoint=False,
+    epochs=1024, # <- 2^16
+    scheduler=True,
+    learning_rate=1e-2,
+    
+    # hand-picked params
+    n_steps=-1,
+    embedding_size=32,
+    batch_size=-1,
+    sequence_length=-1,
+    bagging=True,
+    
+    class_rnn=class_rnn,
+    model=path_model,
+    data=path_data,
+    additional_inputs_data=additional_inputs,
+    
+    # synthetic dataset parameters
+    n_sessions=128,
+    n_trials=200,
+    sigma=0.2,
+    beta_reward=3.,
+    alpha_reward=0.25,
+    alpha_penalty=0.5,
+    forget_rate=0.,
+    confirmation_bias=0.,
+    beta_choice=0.,
+    alpha_choice=1.,
+    counterfactual=False,
+    alpha_counterfactual=0.,
+    
+    save_checkpoints=False,
+    analysis=False,
+    participant_id=0,
+)
+
+train_loss_history, val_loss_history, log_lambda_history, hypergrad_history = histories
+
+plt.figure(figsize=(15,6))
+
+plt.subplot(1,3,1)
+plt.plot(train_loss_history, label="Train Loss")
+plt.plot(val_loss_history, label="Validation Loss")
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(1,3,2)
+plt.plot(log_lambda_history, label='log_lambda')
+# plt.yscale("log")
+plt.xlabel('Epoch')
+plt.ylabel('log_lambda')
+plt.legend()
+
+plt.subplot(1,3,3)
+plt.plot(hypergrad_history, label='Hypergrad')
+plt.xlabel('Epoch')
+plt.ylabel('Hypergrad')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+# input("Press Enter to close plot...")
