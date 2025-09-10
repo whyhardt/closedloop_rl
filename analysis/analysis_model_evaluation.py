317ed99c9b23403c5d78beffd0b1f0ae116e74d3
whyhardt <daniel.weinhardt@uni-osnabrueck.de>
2025-09-10 17:29:02 +0200

    tried a couple of things but nothing yielded better results; open for improvement

--- analysis/analysis_model_evaluation.py
+++ analysis/analysis_model_evaluation.py
@@ -84,13 +84,12 @@ n_actions = 2
 # n_actions = 4
 
 # ------------------------- CONFIGURATION FILE PATHS ------------------------
-use_test = True
-spice_suffix = ''#'_l2_0_0005'
+use_test = False
+spice_suffix = '_elasticnet_l1_0_00001'
 
 path_data = f'data/{study}/{study}.csv'
-path_model_rnn = f'params/{study}/rnn_{study}_elasticnet.pkl'
-# path_model_rnn = f'params/{study}/rnn_{study+spice_suffix}.pkl'
-path_model_spice =None#f'params/{study}/spice_{study+spice_suffix}.pkl'
+path_model_rnn = f'params/{study}/rnn_{study+spice_suffix}.pkl'
+path_model_spice = f'params/{study}/spice_{study+spice_suffix}.pkl'
 path_model_baseline = None#os.path.join(f'params/{study}/', baseline_file)
 path_model_benchmark = None#os.path.join(f'params/{study}', benchmark_file) if len(models_benchmark) > 0 else None
 path_model_benchmark_lstm = f'params/{study}/lstm_{study}.pkl'
